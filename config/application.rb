1e1cf88720f935fb633e06be3d9fefcb73e3138c
あなyui <34minnie@gmail.com>
2024-12-16 11:07:41 +0900

    1

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,11 @@ module Myapp
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
-
+    config.generators do |g|
+      g.skip_routes true
+      g.helper false
+      g.test_framework nil
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
